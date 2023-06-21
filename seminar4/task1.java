bb84c37ab9dd934799b8c9ccfd651b22e80ccd5e
Vlad <brvld@rambler.ru>
2023-06-21 23:02:38 +0300

    task1 created

--- seminar4/task1.java
+++ seminar4/task1.java
@@ -1,2 +1,12 @@
 //Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
 
+/**
+ * task1
+ */
+public class task1 {
+
+    
+    public static void main(String[] args) {
+        
+    }
+}
