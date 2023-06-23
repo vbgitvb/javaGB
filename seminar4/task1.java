17adce4ad9acebb6bce71c2045fc5b849208a49a
Vlad <brvld@rambler.ru>
2023-06-23 19:30:44 +0300

    task1 done

--- seminar4/task1.java
+++ seminar4/task1.java
@@ -1,12 +1,35 @@
 //Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
 
+import java.util.LinkedList;
+
 /**
  * task1
  */
 public class task1 {
 
-    
+    public static void reverseList(LinkedList<Integer> inputList) {
+
+        int lenght = inputList.size();
+        for (int i = 0, j = lenght-1 ; i < lenght/2; i++, j--) {
+            inputList.set(i, inputList.set(j, inputList.get(i)));
+        }
+    }
+
+    public static void fillIntList(LinkedList<Integer> inputList, Integer lenght) {
+        for (int i = 0; i < lenght; i++) {
+            inputList.add(i);
+        }
+    }
+
     public static void main(String[] args) {
-        
+
+        LinkedList<Integer> list = new LinkedList<>();
+        Integer sizeOfList = 10;
+
+        fillIntList(list, sizeOfList);
+        System.out.println(list);
+
+        reverseList(list);
+        System.out.println(list);
     }
 }
