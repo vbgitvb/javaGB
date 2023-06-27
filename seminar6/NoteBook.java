98a3553484e5196898d933a8ba9c6ed11875a1d9
Vlad <brvld@rambler.ru>
2023-06-27 22:15:03 +0300

    class notebook start creat

---
+++ seminar6/NoteBook.java
@@ -0,0 +1,26 @@
+import java.sql.Time;
+
+/**
+ * NoteBook
+ */
+public class NoteBook {
+
+    
+    private long id;
+    private String manufacturer;
+    private String model;
+    private String color;
+    private String displaySize;
+    private String screenResolution;
+    private int memoryCapacity;
+    private int diskCapacity;
+    private String OS;
+    private double price;
+
+
+    public void NoteBook(long id, String manufacturer, String model, String color, String displaySize, String screenResolution, int memoryCapacity, int diskCapacity, String OS, double price) {
+
+        
+    }
+
+}
---
+++ seminar6/task1.java
@@ -0,0 +1,11 @@
+/**
+ * task1
+ */
+public class task1 {
+
+    
+    public static void main(String[] args) {
+        
+        
+    }
+}
