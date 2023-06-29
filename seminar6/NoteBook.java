271a46de0d013599c60f7d95f6af68a108d90434
Vlad <brvld@rambler.ru>
2023-06-29 23:17:29 +0300

    class notebook done

--- seminar6/NoteBook.java
+++ seminar6/NoteBook.java
@@ -1,4 +1,5 @@
 import java.sql.Time;
+import java.util.Objects;
 
 /**
  * NoteBook
@@ -18,9 +19,196 @@ public class NoteBook {
     private double price;
 
 
-    public void NoteBook(long id, String manufacturer, String model, String color, String displaySize, String screenResolution, int memoryCapacity, int diskCapacity, String OS, double price) {
+    public NoteBook(long id, String manufacturer, String model, String color, String displaySize, String screenResolution, int memoryCapacity, int diskCapacity, String OS, double price) {
 
+        this.id = id;
+        if(manufacturer.length() > 0) {
+            this.manufacturer = manufacturer;            
+        } else {
+            this.manufacturer = "Unknown";
+        }
+
+        if(model.length() > 0) {
+            this.model = model;            
+        } else {
+            this.model = "Unknown";
+        }
+
+        this.color = color;
+        
+        if(displaySize.length() > 0) {
+            this.displaySize = displaySize;            
+        } else {
+            this.displaySize = "Unknown";
+        }
+
+        if(screenResolution.length() > 0) {
+            this.screenResolution = screenResolution;            
+        } else {
+            this.screenResolution = "Unknown";
+        }
+
+        this.memoryCapacity = memoryCapacity;
+
+        this.diskCapacity = diskCapacity;
+
+        if(OS.length() > 0) {
+            this.OS = OS;
+        } else {
+            this.OS = "Unknown";
+        }
+
+        if(price > 0) {
+            this.price = price;
+        } else {
+            this.price = 0;
+        }
         
     }
 
+    public long getId() {
+      return id;  
+    } 
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    
+    public String getColor() {
+        return color;       
+    }    
+ 
+    public String getDisplaySize() {
+        return displaySize;    
+    }
+    
+    public String getScreenResolution() {
+        return screenResolution;    
+    }
+    
+    public int getMemoryCapacity() {
+        return memoryCapacity;
+    }
+    
+    public int getDiskCapacity() {
+        return diskCapacity;    
+    }
+    
+    public String getOS() {
+        return OS;    
+    }
+    
+    public double getPrice() {
+        return price;    
+    }
+    
+    public void setId(long id) {
+        if (id > 0) {
+            this.id = id;
+        }
+        else {
+            id = -1;
+        }
+      } 
+      public void setManufacturer(String manufacturer) {
+          if (manufacturer.length() > 0) {
+            this.manufacturer = manufacturer;
+          }
+          else {
+            this.manufacturer = "Unknown";
+          }
+      }
+  
+      public void setModel(String model) {
+          if (model.length() > 0) {
+            this.model = model;
+          }
+          else {
+            model = "Unknown";
+          }
+      }
+      
+      public void setColor(String color) {
+          this.color = color;
+      }    
+   
+      public void setDisplaySize(String displaySize) {
+          if (displaySize.length() > 0 ) {
+            this.displaySize = displaySize;
+          }
+      }
+      
+      public void setScreenResolution(String screenResolution) {
+          if (screenResolution.length() > 0) {
+            this.screenResolution = screenResolution;
+          }
+          else {
+            this.screenResolution = "Unknown";
+          }
+      }
+      
+      public void setMemoryCapacity(int memoryCapacity) {
+          if (memoryCapacity > 0) {
+            this.memoryCapacity = memoryCapacity;
+          } else {
+            memoryCapacity = -1;
+          }
+      }
+      
+      public void setDiskCapacity(int diskCapacity) {
+          if(diskCapacity > 0) {
+            this.diskCapacity = diskCapacity;
+          } else {
+            this.diskCapacity = -1;
+          }
+      }
+      
+      public void setOS(String OS) {
+          if(OS.length() > 0) {
+            this.OS = OS;
+          } else {
+            this.OS = "Unknown";
+          }
+      }
+            
+      public void setPrice(double price) {
+          if (price > 0) {
+            this.price = price;
+          }else {
+            this.price = 0;
+          }
+      }
+
+    @Override
+    public String toString() {
+
+        return "Характеристики устройства: "
+             + "\nid = " + id
+             + "\nПроизводитель: " + manufacturer
+             + "\nМодель: = " + model
+             + "\nЦвет: = " + color
+             + "\nРазмер экрана: = " + displaySize + "\""
+             + "\nРазрешение: = " + screenResolution
+             + "\nОбъём памяти: = " + memoryCapacity + " Mb"
+             + "\nЁмкость диска: = " + diskCapacity + " Mb"
+             + "\nОперационная система: = " + OS
+             + "\nСтоимость: = " + price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null) return false;
+        NoteBook nb = (NoteBook) obj;
+        return manufacturer == nb.manufacturer && model == nb.model && color == nb.color && displaySize == nb.displaySize && screenResolution == nb.screenResolution;
+    }
+
+    @Override
+    public int hashCode() {
+        
+        return Objects.hash(manufacturer, model, color, displaySize, screenResolution);
+    }
 }
--- seminar6/task1.java
+++ seminar6/task1.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /**
  * task1
  */
@@ -6,6 +8,36 @@ public class task1 {
     
     public static void main(String[] args) {
         
+        NoteBook firstNB = new NoteBook(1, "ASUS", "N52", "Черный", "15,6", 
+                        "1280x1024", 4096, 512, "Dos", 6000);
+        NoteBook secondNB = new NoteBook(2, "ASUS", "x30", "Черный", "14", 
+                        "1920x1080", 16384, 512, "Windows 10", 45000);
+
+        NoteBook thirdNB = new NoteBook(1, "ASUS", "N52", "Черный", "15,6", 
+                        "1280x1024", 4096, 512, "Dos", 6000);                        
+
+        System.out.println(firstNB);
+        System.out.println(secondNB);
+
+        if(firstNB.equals(secondNB)) {
+            System.out.println("1 и 2 ноутбуки похожи");
+        }
+            else {
+                System.out.println("1 и 2 ноутбуки разные");
+            }
         
+        if(firstNB.equals(thirdNB)) {
+            System.out.println("1 и 3 ноутбуки похожи");
+        }
+            else {
+                System.out.println("1 и 3 ноутбуки разные");
+            }
+
+        HashSet<NoteBook> nbSet = new HashSet<NoteBook>();
+        nbSet.add(firstNB);
+        nbSet.add(secondNB);
+        nbSet.add(thirdNB);
+
+        System.out.println(nbSet.contains(secondNB));
     }
 }
